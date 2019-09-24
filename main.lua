a7e891348353d26eb0ebffdf8ade81cdffc20cf7
stephaniebang <ej.bang27@gmail.com>
2019-09-24 19:56:46 -0300

    Recalculate when body is outside range and render animated charge

--- main.lua
+++ main.lua
@@ -13,6 +13,23 @@ local function isValidPath(path)
   end
 end
 
+local function setPosition(props)
+  local x, y
+  if props.position.point then
+    x, y = props.position.point:get()
+
+    return { x = x, y = y }
+  end
+
+  local radius = props.body and props.body.size or 8
+
+  repeat
+    x, y = math.random(-1000, 1000), math.random(-1000, 1000)
+  until math.sqrt(x*x + y*y) + radius <= 1000
+  
+  return { x = x, y = y }
+end
+
 function love.load(args)
   math.randomseed(os.time())
 
@@ -44,12 +61,12 @@ function love.load(args)
           props = entityChunk()
         }
 
-        local x, y = 0, 0
+        Entities[index].props.position = setPosition(entity)
 
-        repeat
-          x, y = math.random(-1000, 1000), math.random(-1000, 1000)
-        until math.sqrt(x*x + y*y) <= 1000
-        Entities[index].props.position = { x = x, y = y }
+        if entity.charge then
+          Entities[index].props.charge.a = { x = Entities[index].props.position.x - 8, y = Entities[index].props.position.y, ang = math.pi }
+          Entities[index].props.charge.b = { x = Entities[index].props.position.x + 8, y = Entities[index].props.position.y, ang = 0 }
+        end
 
         index = index + 1
       end
@@ -82,10 +99,10 @@ function love.draw()
     if props.control then
       love.graphics.setColor(1, 1, 1)
     elseif props.field then
-      if props.field.strength < 0 then love.graphics.setColor(1, 0, 0)
-      elseif props.field.strength > 0 then love.graphics.setColor(0, 0, 1) end
+      if props.field.strength < 0 then love.graphics.setColor(.9, .3, .3)
+      elseif props.field.strength > 0 then love.graphics.setColor(.3, .3, .9) end
     else
-      love.graphics.setColor(0, 1, 0)
+      love.graphics.setColor(.3, .9, .3)
     end
 
     -- Draw entity
@@ -99,20 +116,44 @@ function love.draw()
 
     -- Draw field
     if props.field then
-      if not props.body then
-        if props.field.strength < 0 then love.graphics.setColor(1, 0, 0)
-        elseif props.field.strength > 0 then love.graphics.setColor(0, 0, 1)
+      if props.field.strength < 0 then love.graphics.setColor(1, 0, 0)
+      elseif props.field.strength > 0 then love.graphics.setColor(0, 0, 1)
       else love.graphics.setColor(0, 1, 0) end
-      else
-        love.graphics.setColor(1, 1, 1)
-      end
 
       love.graphics.circle('line', x, y, math.abs(Entities[i].props.field.strength))
+    end
+
+    -- Draw charge
+    if props.charge then
+      if props.charge.strength < 0 then love.graphics.setColor(1, 0, 0)
+      elseif props.charge.strength > 0 then love.graphics.setColor(0, 0, 1)
+      else love.graphics.setColor(0, 1, 0) end
+
+      love.graphics.circle('line', props.charge.a.x, props.charge.a.y, 4)
+      love.graphics.circle('line', props.charge.b.x, props.charge.b.y, 4)
     end
 
     -- DELETAR !!!!!!
     love.graphics.setColor(1, 1, 1)
     love.graphics.print(Entities[i].name, x, y + 10)
+  end
+end
+
+local function updateCharge(x, y, ang, q, dt)
+  local freq = math.sqrt(math.abs(q))
+  local angle = freq*dt*2*math.pi + ang
+
+  return { x = x + math.sin(angle)*8, y = y + math.cos(angle)*8, ang = angle }
+end
+
+function love.update(dt)
+  for i=1, #Entities do
+    if Entities[i].props.charge then
+      local charge = Entities[i].props.charge
+      local pos = Entities[i].props.position
+      charge.a = updateCharge(pos.x, pos.y, charge.a.ang, charge.strength, dt)
+      charge.b = updateCharge(pos.x, pos.y, charge.b.ang, charge.strength, dt)
+    end
   end
 end
 
